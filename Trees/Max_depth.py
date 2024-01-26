5d7b56925cdc26280f10e1e17ea72816f65733a0
Hirdanshu <65655254+hirdanshu90@users.noreply.github.com>
2024-01-26 15:35:55 +0900

    Update Max_depth.py

--- Trees/Max_depth.py
+++ Trees/Max_depth.py
@@ -1,8 +1,22 @@
-# Max depth/height measures the maximum downward distance from the root to a leaf.
+# Max depth/height measures the maximum downward distance from the root to a leaf. (Basically max number of nodes along the way so we add 1, when doing recurrsively.)
+
+# Can be solved using 3 approaches........
+
+
+# APPROACH 1 : Recurrsive DFS
 
 def maxDepth(self, root: Optional[TreeNode]) -> int:
 
+        # Base Case
         if root is None: 
             return 0
 
         return max(self.maxDepth(root.left), self.maxDepth(root.right)) + 1
+
+
+# APPROACH 2 : Without Recurrsion: 
+
+# BFS Iteratively: ( Level Order Solution )
+        
+# Here we calculate the number of levels == Depth of the Tree. 
+# We solve BFS by using Queue data structure.
