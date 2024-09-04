cc4fd36a21ec93c55974add29c77b9f734e227dd
Hirdanshu <65655254+hirdanshu90@users.noreply.github.com>
2024-09-04 09:18:47 +0900

    Adding polymorphism

--- OOPS in python/Rough.py
+++ OOPS in python/Rough.py
@@ -1,15 +1,6 @@
-class Instructor():
-    height = 5
-    def __init__(self,name, age):
-        self.name = name
-        self.age = age
-        self.sport = None
-    def name_age(self, cash):
-        return f"name and age are: {self.name} and {self.age} and cash hai {cash}"
-    def school(self, school):
-        return f"school hai {school}"
+class Demo():
+    name = "Jenny"
 
-
-instructor_1 = Instructor("Jenny", 44)
-print(instructor_1.name_age(10000))
-print(instructor_1.school("Infosys"))
+d = Demo()
+print(len(d.name))
+ 
